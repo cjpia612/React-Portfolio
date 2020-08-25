3c8246fbbf2febc401373790c4c33b8df2b92926
Cattleya Pia <cjpia612@gmail.com>
2020-08-24 23:34:08 -0700

    added dependencies, created functional Navbar component

---
+++ cj-portfolio/src/components/Navibar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const Navibar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">CJ Pia's Portfolio</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Navibar;
