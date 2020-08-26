ef5fa1404a5db1c69594cc7ca0327b0f5deb920f
Cattleya Pia <cjpia612@gmail.com>
2020-08-25 23:06:38 -0700

    added footer component and edited app,js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer'
-
-const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
-  );
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
