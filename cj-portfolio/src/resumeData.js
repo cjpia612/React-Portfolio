2c0c22ade4f539a5f7af818620afb597645d23ee
Cattleya Pia <cjpia612@gmail.com>
2020-08-25 22:55:48 -0700

    added about,portfolio, footer, resume components and created resumeData object

---
+++ cj-portfolio/src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rahul Bhatia",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/cattleya-pia/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/cjpia612",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/itzceejaayyy",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"My name is Cattleya Jade Pia, but I go by CJ. I am currently working as an Administrative Assistant in LA's largest healthcare system and pursuing certification in Full-Stack Software Development. I graduated from Mount Saint Mary's University with a B.A. in Healthcare Policy. My professional interests vary from public policy, new media, and app/game development. am a growing Software Development Engineer. I am currently enrolled in UCLA Extension's Full Stack Software Development course. I am all at once excited and anxious &#40; okay, EXTREMELY anxious &#41;, but I am continuing to push through and improve my skills every step of the way. Feel free to connect with me below!",
+    "address":"USA",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"UCLA Extension",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Mount St. Mary's University",
+        "specialization":"Healthcare Policy",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2017",
+        "Achievements":"NSLS member"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"UCLA Health",
+        "specialization":"Administrative Assistant II",
+        "MonthOfLeaving":"April 2018",
+        "YearOfLeaving":"Present",
+        "Achievements":" "
+      },
+      {
+        "CompanyName":"AerioConnect",
+        "specialization":"Executive Assistant",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
