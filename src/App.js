f7e0012f00a3e4180a032110787b4b3d4ec56d63
Cattleya Pia <cjpia612@gmail.com>
2020-08-25 22:57:51 -0700

    moved all files and folders into root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
