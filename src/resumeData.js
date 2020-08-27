ae6a3abc8058630b0d561b180ce7c2dfece4be18
Cattleya Pia <cjpia612@gmail.com>
2020-08-27 16:59:09 -0700

    edited w/ personal information

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,53 +1,53 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    // "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "CJ Pia",
+    "role": "Full Stack Developer",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/cattleya-pia/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/cjpia612",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/itzceejaayyy",
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "aboutme":"I am a progressing full stack developer interested in creating meaningful and immersive experiences. After having received a certificate in full stack web development from the UCLA Extension program, I am confident in my HTML, CSS, and Javascript abilities and always produce “clean” code. Professionally, I have accumulated 2 letters of recommendation from clients after helping develop websites to increase their presence. Within development, my goal is to always achieve a meaningful and immersive experience to develop solutions for meaningful projects. Developing for companies/groups focused on social awareness, entertainment, game development, and political movements are motivators for me to continue in enhancing my technical skills and abilities.",
+    "address":"Los Angeles, CA",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
+        "UniversityName":"Mount St. Mary's University",
+        "specialization":"Healthcare Policy",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
         "Achievements":"Some Achievements"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
+        "UniversityName":"UCLA Extension",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"March 2020",
+        "YearOfPassing":"to September 2020",
         "Achievements":"Some Achievements"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"UCLA Health",
+        "specialization":"Administrative Assistant II",
+        "MonthOfLeaving":"April 2018",
+        "YearOfLeaving":"to Present",
+        "Achievements":" "
       },
       {
         "CompanyName":"Some Company",
@@ -71,12 +71,12 @@ let resumeData = {
     ],
     "portfolio":[
       {
-        "name":"project1",
+        "name":"Law Offices of Theodore Behlendorf",
         "description":"mobileapp",
         "imgurl":"images/portfolio/phone.jpg"
       },
       {
-        "name":"project2",
+        "name":"Soma Kitchen",
         "description":"mobileapp",
         "imgurl":"images/portfolio/project.jpg"
       },
